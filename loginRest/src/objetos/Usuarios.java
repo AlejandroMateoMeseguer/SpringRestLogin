a694396f7344cfbe7011815d5c52d6d397987d11
AlejandroMateoMeseguer <a.mateomeseguer@indizen.com>
2021-09-22 17:01:45 +0200

    objeto user creado

---
+++ loginRest/src/objetos/Usuarios.java
@@ -0,0 +1,86 @@
+package objetos;
+
+import java.beans.JavaBean;
+
+@JavaBean
+public class Usuarios {
+	int id;
+	String nombre;
+	String apellido;
+	String banco;
+	String nick;
+	String sexo;
+	int edad;
+
+	public Usuarios(int id, String nombre, String apellido, String banco, String nick, String sexo, int edad) {
+
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.banco = banco;
+		this.nick = nick;
+		this.sexo = sexo;
+		this.edad = edad;
+	}
+
+	public Usuarios() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+}
